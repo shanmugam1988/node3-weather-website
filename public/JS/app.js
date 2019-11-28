d4dba38973743b87f4dbbf4b8e583a21ac32e602
shanmugam1988 <shanmugasundaram.99@gmail.com>
2019-11-28 14:30:06 +0530

    removed unnecessary console.log call

--- public/JS/app.js
+++ public/JS/app.js
@@ -1,4 +1,4 @@
-console.log('Javascript file loaded.')
+//console.log('Javascript file loaded.')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
